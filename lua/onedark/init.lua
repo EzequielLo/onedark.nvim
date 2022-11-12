a3b55bb8523e708129bc99c8e5dd431c4a7594d3
Ezequiel Lo Iacono <ezequiel_lo_iacono@protonmail.com>
2022-11-12 07:43:21 +0100

    change keywords

--- lua/onedark/init.lua
+++ lua/onedark/init.lua
@@ -39,7 +39,7 @@ local highlights = {
   { hg = 'Repeat', fg = colors.purple }, --for, do, while, etc.
   { hg = 'Label', fg = colors.purple }, --case, default, etc.
   { hg = 'Operator', fg = colors.purple }, --sizeof", "+", "*", etc.
-  { hg = 'Keyword', fg = colors.red }, --any other keyword
+  { hg = 'Keyword', fg = colors.purple }, --any other keyword
   { hg = 'Exception', fg = colors.purple }, --try, catch, throw
   { hg = 'PreProc', fg = colors.yellow }, --generic Preprocessor
   { hg = 'Include', fg = colors.blue }, --preprocessor #include
